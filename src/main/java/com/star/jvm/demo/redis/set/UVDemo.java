cd09a47255fe9c3391be380d42af8631a0b757dd
YoungStar9527 <18146702725@163.com>
2021-06-14 15:40:50 +0800

    123

--- src/main/java/com/star/jvm/demo/redis/set/UVDemo.java
+++ src/main/java/com/star/jvm/demo/redis/set/UVDemo.java
@@ -1,16 +1,45 @@
 package com.star.jvm.demo.redis.set;
 
-import redis.clients.jedis.Jedis;
+import redis.clients.jedis.HostAndPort;
+import redis.clients.jedis.JedisCluster;
+import redis.clients.jedis.JedisPoolConfig;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * 网站uv统计案例
  */
 public class UVDemo {
 
-    private Jedis jedis = new Jedis("127.0.0.1");
+    private static JedisCluster jedis;
+    static {
+        // 添加集群的服务节点Set集合
+        Set<HostAndPort> hostAndPortsSet = new HashSet<HostAndPort>();
+        // 添加节点
+        hostAndPortsSet.add(new HostAndPort("192.168.31.101", 7001));
+        hostAndPortsSet.add(new HostAndPort("192.168.31.101", 7002));
+        hostAndPortsSet.add(new HostAndPort("192.168.31.102", 7003));
+        hostAndPortsSet.add(new HostAndPort("192.168.31.102", 7004));
+        hostAndPortsSet.add(new HostAndPort("192.168.31.103", 7005));
+        hostAndPortsSet.add(new HostAndPort("192.168.31.103", 7006));
+
+        // Jedis连接池配置
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+        // 最大空闲连接数, 默认8个
+        jedisPoolConfig.setMaxIdle(100);
+        // 最大连接数, 默认8个
+        jedisPoolConfig.setMaxTotal(500);
+        //最小空闲连接数, 默认0
+        jedisPoolConfig.setMinIdle(0);
+        // 获取连接时的最大等待毫秒数(如果设置为阻塞时BlockWhenExhausted),如果超时就抛异常, 小于零:阻塞不确定的时间,  默认-1
+        jedisPoolConfig.setMaxWaitMillis(2000); // 设置2秒
+        //对拿到的connection进行validateObject校验
+        jedisPoolConfig.setTestOnBorrow(true);
+        jedis = new JedisCluster(hostAndPortsSet, jedisPoolConfig);
+    }
 
     /**
      * 添加一次用户访问记录
